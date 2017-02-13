7cdc96f2a8d10c91eec6ab3f755ecbf8e1c7491a
J2D2Development <joedriscoll79@gmail.com>
2017-02-12 17:07:35 -0800

    Started working on auth using passport- need to rethink routes

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ const helmet = require('helmet');
 const connection = require('./db');
 const session = require('express-session');
 // const bcrypt = require('bcrypt');
-//const config = require('./config');
+
+
+//passport module - auth system
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    connection.query(`SELECT * FROM hoa_user WHERE hoa_id=${user['hoa_id']} AND username='${user["username"]}'`, (error, results) => {
+        if(error) { 
+            done(error);
+        } else {
+            done(null, results[0]);
+        }
+    });
+});
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -25,6 +41,8 @@ app.use(session({
     secret: 'SOMERANDOMSECRETHERE', 
     cookie: { maxAge: 60000 }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.disable('x-powered-by');
 
@@ -38,55 +56,127 @@ app.use((req, res, next) => {
             sitename = req.headers.host;
         }
         get_hoa_main();
-    } else if(req.originalUrl.split('/').length > 1 && req.originalUrl.split('/')[1] !== 'favicon.ico') {
+    } else if(req.originalUrl.split('/').length > 1 && 
+        req.originalUrl.split('/')[1] !== 'favicon.ico' && 
+        req.originalUrl.split('/')[1] !== 'login-adminpost' &&
+        req.originalUrl.split('/')[1] !== 'logout') {
         const urlArray = req.originalUrl.split('/');
         sitename = urlArray[1];
         get_hoa_main();
+    } else {
+        next();
     }
 
     function get_hoa_main() {
         connection.query(`select * from hoa_main where hoa_id_name = '${sitename}'`, (error, results) => {
-            if(error || results.length !== 1) {
-                req.session.sitename = 'unavailable';
-                req.session['short_name'] = 'Not Found';
-                next();
+            if(error || results.length === 0) {
+                req.session.sitename = sitename;
+                res.status(404).render('templates-error/site-not-found.ejs', {
+                    submittedSitename: req.session.sitename
+                });
             } else {
                 req.session.sitename = results[0]['hoa_id_name'];
                 req.session['hoa_main'] = results[0];
-                get_hoa_lookfeel(results[0]['hoa_id']);
+                get_hoa_main_aux(results[0]['hoa_id']);
+            }
+        });
+    }
+
+    function get_hoa_main_aux(hoa_id) {
+        connection.query(`SELECT * FROM hoa_main_aux WHERE hoa_id = ${hoa_id}`, (error, results) => {
+            if(error || results.length === 0) {
+                res.status(404).render('templates-error/site-not-found.ejs', {
+                    submittedSitename: req.session.sitename
+                });
+            } else {
+                req.session['hoa_main_aux'] = results[0];
+                get_hoa_lookfeel(hoa_id);
             }
         });
     }
 
     function get_hoa_lookfeel(hoa_id) {
         connection.query(`SELECT * FROM hoa_pub_lookfeel WHERE hoa_id = ${hoa_id}`, (error, results) => {
-            if(error || results.length !== 1) {
-                throw error;
+            if(error || results.length === 0) {
+                res.status(404).send('Not found!');
+                //might change this to 'no published look yet' error
             } else {
                 req.session['hoa_lookfeel'] = results[0];
                 next();
             }
         });
     }
+
+
 });
+
+
+passport.use(new LocalStrategy(
+    { passReqToCallback: true },
+    (req, username, password, done) => {
+        const hoa_id = req.session['hoa_main']['hoa_id'];
+        connection.query(`SELECT * FROM hoa_user WHERE hoa_id=${hoa_id} AND username='${username}'`, (error, results) => {
+            console.log('results in strat:', results[0]);
+            console.log('name:', results[0].first_name);
+            if(error) {
+                return done(error);
+            } else if(!results || results[0].length === 0) {
+                return done(null, false, { message: 'No user found!' });
+            } else {
+                return done(null, results[0]);
+            }
+        });
+    }
+));
 
 app.get('/', (req, res, next) => {
     console.log('homepage redirect');
     res.redirect(`${req.session['sitename']}/page`);
 });
 
-app.get('/logout', (req, res, next) => {
-    req.session.destroy(err => {
-        if(err) console.log('session destroy error:', err);
-        res.redirect('/');
-    });
+//for some reason this redirects too many times.
+// app.get('/site-not-found', (req, res) => {
+//     res.status(404).render('templates-error/site-not-found.ejs', {
+//         submittedSitename: req.session.sitename
+//     });
+// });
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
-app.get('/unavailable', (req, res, next) => {
+
+//admin login routes?
+app.post('/login-adminpost', (req, res, next) => {
+    console.log('posting to admin login:', req.session['sitename']);
+    passport.authenticate('local', 
+    { 
+        successRedirect: `/${req.session['sitename']}/admin`,
+        failureRedirect: `/${req.session['sitename']}/login-admin`,
+        failureFlash: true
+    })(req, res, next);
+});
+
+app.get('/:sitename/login-admin', (req, res) => {
+    if(req.user) {
+        res.redirect(`/${req.session['sitename']}/admin`);
+    } else {
+        res.render('cp/login-admin.ejs', {
+            sitename: req.session['sitename']
+        });
+    }
+});
+
+app.get('/unavailable', (req, res) => {
     req.session.destroy(err => {
         if(err) console.log('session destroy error:', err);
         res.render('db-down.ejs');
     });
+});
+
+app.get('/admin', (req, res) => {
+    res.redirect(`${req.session['sitename']}/admin`);
 });
 
 app.get('/:sitename', (req, res, next) => {
@@ -94,8 +184,17 @@ app.get('/:sitename', (req, res, next) => {
     res.redirect(`${req.session['sitename']}/page`);
 });
 
+app.get('/:sitename/admin', (req, res) => {
+    console.log(req.user);
+    res.render('cp/index-admin.ejs', {
+        sitename: req.session['sitename'],
+        user: req.user
+    });
+});
+
 app.get('/:sitename/page', (req, res, next) => {
     const hoa_main = req.session['hoa_main'];
+    const hoa_main_aux = req.session['hoa_main_aux'];
     const hoa_lookfeel = req.session['hoa_lookfeel'];
     const page_id = +req.query['page_id'] || 1;
     get_page_info(page_id);
@@ -106,25 +205,37 @@ app.get('/:sitename/page', (req, res, next) => {
                 //-use connection.escape(var) ?
             //http://stackoverflow.com/questions/6622746/approach-to-multiple-mysql-queries-with-node-js
             //http://www.thegeekstuff.com/2014/01/mysql-nodejs-intro/?utm_source=tuicool
-        connection.query(`SELECT * FROM hoa_pub_page WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${pageid};SELECT * FROM hoa_pub_page_area WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${pageid}`, (error, results) => {
+        connection.query(`SELECT * FROM hoa_pub_page WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(pageid)}; SELECT * FROM hoa_pub_page_area WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(pageid)}; SELECT * FROM hoa_pub_menuitem WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(pageid)};`, (error, results) => {
             if(error) {
                 throw error;
             } else {
-                //console.log('first:', typeof results[0], results[0][0]['title']);
-                const pageInfo = results[0][0];
-                res.render('index.ejs', {
-                    short_name: hoa_main['short_name'], 
-                    full_name: hoa_main['short_name'],
-                    hoa_main: hoa_main,
-                    hoa_lookfeel: hoa_lookfeel,
-                    title: `${pageInfo['title']}`,
-                    pageAreas: results[1]
-                });
+                if(results[0].length === 0) {
+                    res.status(404).send('Not found!');
+                } else {
+                    const pageInfo = results[0][0];
+                    const title = req.query['page_id'] && req.query['page_id'] !== 1 ? pageInfo['title'] : hoa_main['short_name'];
+                    const template = templates[hoa_lookfeel['nav_orientation']];
+
+                    res.render(template, {
+                        hoa_main: hoa_main,
+                        hoa_main_aux: hoa_main_aux,
+                        hoa_lookfeel: hoa_lookfeel,
+                        title: title,
+                        pageAreas: results[1],
+                        menuItems: results[2]
+                    });
+                }
             }
         })
     }
 });
 
+
+//testing templates: get lookfeel value and render different template (could extend this to have more template options if it works)
+const templates = {
+    "v": "index-vertical-basic.ejs",
+    "h": "index-horizontal-basic.ejs"  
+};
 
 const server = app.listen(80, '127.0.0.1', function() {
 	console.log('Server started!');
