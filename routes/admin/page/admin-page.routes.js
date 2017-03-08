8c48f7ca23b398726cee0b02c472c59d2ffa7fe9
J2D2Development <joedriscoll79@gmail.com>
2017-03-08 06:51:33 -0800

    Updated menu item edit form to filter data by type when submitting for put

--- routes/admin/page/admin-page.routes.js
+++ routes/admin/page/admin-page.routes.js
@@ -11,88 +11,27 @@ const templates = require('../../../templates');
 pageRouter.route('/')
     .get((req, res) => {
         const hoa_id = req.session['hoa_main']['hoa_id'];
-        const page_id = 1;
         const sitename = req.session['sitename'];
-
         const hoa_main = req.session['hoa_main'];
         const hoa_main_aux = req.session['hoa_main_aux'];
-        const hoa_lookfeel = req.session['hoa_lookfeel'];
 
-        Promise.all([
-            basicUtils.getDBInfo(`SELECT * FROM hoa_pub_page WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`), 
-            basicUtils.getDBInfo(`SELECT * FROM hoa_pub_page_area WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`), 
-            basicUtils.getDBInfo(`SELECT * FROM hoa_pub_menuitem WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`),
-            basicUtils.getDBInfo(`SELECT * FROM hoa_feature WHERE hoa_id = ${hoa_main['hoa_id']};`),
-            basicUtils.getDBInfo(`SELECT * FROM hoa_feature_item WHERE hoa_id = ${hoa_main['hoa_id']};`),
-            pageUtils.getPageList(hoa_id)
-        ])
+        pageUtils.getPageList(hoa_id)
         .then(results => {
-            if(results[0].length === 0) {
+            console.log(results);
+            if(results.length === 0) {
                 return res.status(404).send('Not found!');
             } else {
-                const pageInfo = results[0];
-                const pageAreas = results[1];
-                const featureList = results[3];
-                const featureItemList = results[4];
-                const pageList = results[5];
-
-                const title = req.query['page_id'] && req.query['page_id'] !== 1 ? pageInfo['title'] : hoa_main['short_name'];
-                const template = templates[hoa_lookfeel['nav_orientation']].template;
-                //const folder = templates[hoa_lookfeel['nav_orientation']].folder;
-                const currentPage = pageList.find(p => p.page_id === page_id);
-
-                //insert selected prop on current page
-                pageList.forEach(page => {
-                    page.selected = page.page_id === currentPage.page_id;
+                const pageList = results;
+                return res.render('admin/pages-main.ejs', {
+                    hoa_main,
+                    hoa_main_aux,
+                    sitename,
+                    pageList
                 });
-
-                //if menu item is graphical, get the background image
-                //!!! need to add a new function to handle the menuItems array gen- call it in this block as well as below?
-                if(hoa_lookfeel['menu_button_type'] === 'bg_image') {
-                    const button_id = hoa_lookfeel['menu_button_id'];
-                    basicUtils.getGraphicalMenuItemImage(button_id)
-                        .then(button => {
-                            //using graphic menu items- got item, now generate info
-                            const menuItems = basicUtils.getMenuItems(results[2]);
-                            const buttonBgInfo = {
-                                buttonBgImg: `/static/images/${button[0].normal_file}`,
-                                buttonHoverImg: `/static/images/${button[0].hover_file}`,
-                                width: `${button[0].file_width}px`,
-                                height: `${button[0].file_height}px`,
-                                padding: `0 ${button[0].pad_right}px 0 ${button[0].pad_left}px`
-                            };
-                            return res.render('admin/pages.ejs', {
-                                main_page_template: `./${template}`,
-                                hoa_main,
-                                hoa_main_aux,
-                                hoa_lookfeel,
-                                title,
-                                pageAreas,
-                                menuItems,
-                                buttonBgInfo,
-                                sitename,
-                                pageList,
-                                featureList,
-                                featureItemList,
-                                currentPage
-                            });
-                        })
-                        .catch(error => {
-                            console.log('Error getting button bg image:', error);
-                        });
-                } else {
-                    //not using graphical menu items, don't need to fetch bg image
-                    let menuItems = basicUtils.getMenuItems(results[2]);
-                    return res.render(template, {
-                        hoa_main,
-                        hoa_main_aux,
-                        hoa_lookfeel,
-                        title,
-                        pageAreas,
-                        menuItems
-                    });
-                }
             }
+        })
+        .catch(error => {
+            console.log('Error getting button bg image:', error);
         });
     });
     //.post and so on... - this .post would be to add new page?
@@ -112,8 +51,7 @@ pageRouter.route('/:page_id')
 
         Promise.all([
             basicUtils.getDBInfo(`SELECT * FROM hoa_pub_page WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`), 
-            basicUtils.getDBInfo(`SELECT * FROM hoa_pub_page_area WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`), 
-            basicUtils.getDBInfo(`SELECT * FROM hoa_pub_menuitem WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`),
+            basicUtils.getDBInfo(`SELECT * FROM hoa_pub_page_area WHERE hoa_id = ${hoa_main['hoa_id']} AND page_id = ${connection.escape(page_id)};`),
             basicUtils.getDBInfo(`SELECT * FROM hoa_feature WHERE hoa_id = ${hoa_main['hoa_id']} AND feature_id != 8;`),
             basicUtils.getDBInfo(`SELECT * FROM hoa_feature_item WHERE hoa_id = ${hoa_main['hoa_id']};`),
             pageUtils.getPageList(hoa_id)
@@ -124,10 +62,9 @@ pageRouter.route('/:page_id')
             } else {
                 const pageInfo = results[0];
                 const pageAreas = results[1];
-                const menuItems = results[2];
-                const featureList = results[3];
-                const featureItemList = results[4];
-                const pageList = results[5];
+                const featureList = results[2];
+                const featureItemList = results[3];
+                const pageList = results[4];
 
                 const title = req.query['page_id'] && req.query['page_id'] !== 1 ? pageInfo['title'] : hoa_main['short_name'];
                 const template = templates[hoa_lookfeel['nav_orientation']].template;
@@ -159,7 +96,6 @@ pageRouter.route('/:page_id')
                                 hoa_lookfeel,
                                 title,
                                 pageAreas,
-                                menuItems,
                                 buttonBgInfo,
                                 sitename,
                                 pageList,
@@ -220,9 +156,15 @@ pageRouter.route('/:page_id/menuitems')
     })
     .post((req, res) => {
         //add new menu item
-    })
-    .put((req, res) => {
-        //update menu item- but how to get the id?
-    })
+    });
+
+    pageRouter.route('/:page_id/menuitems/:menu_item_id')
+        .put((req, res) => {
+            //update existing menu item
+        })
+        .delete((req, res) => {
+            //delete existing menu item
+        });
+
 
 module.exports = pageRouter;
