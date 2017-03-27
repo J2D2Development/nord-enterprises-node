ce53ef9c28917cd9e734260bc62aa6a48c3d5746
J2D2Development <joedriscoll79@gmail.com>
2017-03-27 08:57:28 -0700

    Working on main page list admin view: display current settings for each

--- static/js/src/admin/page-list-card.js
+++ static/js/src/admin/page-list-card.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import moment from 'moment';
 
-const PageListCard = (props) => {
+const PageListCard = (props) => {    
     return (
         <div className="col-md-4 col-sm-6 col-xs-12">
             <div className="pagelist-inner">
-                <h3>{props.data.title}</h3>
+                <h3>
+                    {props.data.title}
+                    <em><small>
+                        {props.data.home_page === 'y' ? '  [Homepage]' : ''}
+                    </small></em>
+                </h3>
                 <div className="pagelist-main">
-                    Page desc of some kind?
+                    <div className="pagelist-main_row">
+                        <div>Require Password</div>
+                        <div>
+                            <i className={"fa " + (props.data.require_auth === 'y' ? 'fa-check' : 'fa-ban')} aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div className="pagelist-main_row">
+                        <div>Page Admin</div>
+                        <div>
+                            {(props.data.admin_user && props.data.admin_user !== '0') ? props.data.admin_user : 'None Assigned'}
+                        </div>
+                    </div>
+                    <div className="pagelist-main_row">
+                        <div>Include Table of Contents</div>
+                        <div>
+                            <i className={"fa " + (props.data.incl_toc === 'y' ? 'fa-check' : 'fa-ban')} aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div className="pagelist-main_row">
+                        <div>Last Updated By </div>
+                        <div>{props.data.updt_user}</div>
+                    </div>
+                    <div className="pagelist-main_row">
+                        <div>Last Updated On</div>
+                        <div>{moment(props.data.updt_dttm).format('MMMM Do YYYY')}</div>
+                    </div>
                 </div>
                 <div className="pagelist-links">
                     <a onClick={() => props.openModal(props.data)}>Edit Page Basics</a>
