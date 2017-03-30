c38ba6077fd119cea88353dd10df84bd3c4ca80b
J2D2Development <joedriscoll79@gmail.com>
2017-03-30 08:38:11 -0700

    Added test-babel to allow import in testing files

--- test/first_test.js
+++ test/first_test.js
@@ -1,9 +1,21 @@
+/* eslint-env node, mocha */
 const assert = require('assert');
+require("babel-register");
+
+import { utilities } from '../static/js/src/admin/utilities';
 
 describe('Test feature', () => {
     describe('Test scenario', () => {
         it('Should work', () => {
             assert(true);
+        });
+    });
+});
+
+describe('Utilities Tests', () => {
+    describe('Test SEtup', () => {
+        it('Should have a hideLoader function', () => {
+            assert.equal(typeof utilities.hideLoader, 'function');
         });
     });
 });
